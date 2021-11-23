6b201224c56d2ba616c10ffca3b0a353ca252ec1
Sergey Obidin <player181285@gmail.com>
2021-11-23 17:22:54 +0400

    added new code and test

---
+++ src/main/java/ru/netogy/domain/Radio.java
@@ -0,0 +1,104 @@
+package ru.netogy.domain;
+
+public class Radio {
+    
+    private int radioStation;
+    private int nextStation;
+    private int prevStation;
+    private int increaseVolume;
+    private int decreaseVolume;
+
+    public int getRadioStation() {
+        return radioStation;
+    }
+
+    public void setRadioStation(int radioStation) {
+        if (radioStation < 0) {
+            return;
+        }
+        if (radioStation > 9) {
+            return;
+        }
+        this.radioStation = radioStation;
+    }
+
+    public int getIncreaseVolume() {
+        return increaseVolume;
+    }
+
+    public void setIncreaseVolume(int increaseVolume) {
+
+        if (increaseVolume >= 10) {
+            increaseVolume = 10;
+        }
+        if (increaseVolume < 0) {
+            return;
+        }
+        if (increaseVolume < 10) {
+            increaseVolume = increaseVolume + 1;
+        }
+
+        this.increaseVolume = increaseVolume;
+    }
+
+    public int getDecreaseVolume() {
+        return decreaseVolume;
+    }
+
+    public void setDecreaseVolume(int decreaseVolume) {
+        if (decreaseVolume <= 0) {
+            decreaseVolume = 0;
+        }
+        if (decreaseVolume > 10) {
+            return;
+        }
+        if (decreaseVolume > 0) {
+            decreaseVolume = decreaseVolume - 1;
+        }
+        this.decreaseVolume = decreaseVolume;
+    }
+
+    public int getNextStation() {
+        return nextStation;
+    }
+
+    public void setNextStation(int nextStation) {
+        if (nextStation < 0) {
+            return;
+        }
+        if (nextStation > 9) {
+            return;
+        }
+        if (nextStation == 9) {
+            nextStation = -1;
+        }
+        if (nextStation < 9) {
+            nextStation++;
+        }
+
+        this.nextStation = nextStation;
+    }
+
+    public int getPrevStation() {
+        return prevStation;
+    }
+
+    public void setPrevStation(int prevStation) {
+        if (prevStation < 0) {
+            return;
+        }
+        if (prevStation > 9) {
+            return;
+        }
+        if (prevStation == 0) {
+            prevStation = 10;
+        }
+        if (prevStation > 0) {
+            prevStation--;
+        }
+        this.prevStation = prevStation;
+    }
+
+
+}
+
---
+++ src/test/java/ru/netogy/domain/RadioTest.java
@@ -0,0 +1,54 @@
+package ru.netogy.domain;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RadioTest {
+
+    @Test
+    public void shouldSetNameStation() {
+        Radio radio = new Radio();
+        radio.setNameStation("Номер станции");
+        assertEquals("Номер станции", radio.getNameStation());
+    }
+
+    @Test
+    public void shouldSetRadioStation() {
+        Radio radio = new Radio();
+        radio.setRadioStation( 0 );
+        assertEquals(0, radio.getRadioStation());
+    }
+
+
+    @Test
+    public void shouldSetIncreaseVolume(){
+        Radio radio = new Radio();
+        radio.setIncreaseVolume(5);
+        assertEquals(6, radio.getIncreaseVolume());
+
+    }
+
+    @Test
+    public void shouldSetIDecreaseVolume(){
+        Radio radio = new Radio();
+        radio.setDecreaseVolume(5);
+        assertEquals(4, radio.getDecreaseVolume());
+    }
+
+    @Test
+    public void shouldSetNextStation() {
+        Radio radio = new Radio();
+        radio.setNextStation(9);
+        assertEquals(4, radio.getNextStation());
+    }
+
+    @Test
+    public void shouldSetPrevStation() {
+        Radio radio = new Radio();
+        radio.setPrevStation(6);
+        assertEquals(4, radio.getPrevStation());
+    }
+
+
+}
